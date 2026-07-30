d1877b95f6fcca24ef8004adf2e43fed075a2f3f
legendvk1176 <legendvk1176@gmail.com>
2026-07-30 07:40:56 +0530

    main.py

--- main.py
+++ main.py
@@ -433,7 +433,7 @@ async def help_command(client: Client, msg: Message):
     await msg.reply_text(help_text)
 
 # Upload command handler
-@bot.on_message(filters.command(["Vishal"]))
+@bot.on_message(filters.command(["VK"]))
 async def upload(bot: Client, m: Message):
     if not is_authorized(m.chat.id):
         await m.reply_text("**🚫You are not authorized to use this bot.**")
